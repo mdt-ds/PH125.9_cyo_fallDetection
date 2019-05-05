28111e9e69e230ef1b56efe5043a16671408257f
De Toma <mario.de-toma@dxc.com>
2019-05-05 19:49:15 +0200

    first commit

---
+++ PH125_9_cyo_capstone_script_mdt.R
@@ -0,0 +1,257 @@
+# Script Header ----
+# File-Name:      PH125_9_cyo_capstone_script_mdt.R
+# Date:           March 31, 2019                                   
+# Author:         Mario De Toma <mdt.datascience@gmail.com>
+# Purpose:        R script for submission of PH125_9 choose your own capstone project for
+#                 HarvardX Data Science Professional Certificate
+# Data Used:      falldetection dataset   
+# Packages Used:  dplyr, caret, ggplot2, doSNOW, e1071  
+
+# This program is believed to be free of errors, but it comes with no guarantee! 
+# The user bears all responsibility for interpreting the results.
+
+# All source code is copyright (c) 2017, under the Simplified BSD License.  
+# For more information on FreeBSD see: http://www.opensource.org/licenses/bsd-license.php
+
+# All images and materials produced by this code are licensed under the Creative Commons 
+# Attribution-Share Alike 3.0 United States License: http://creativecommons.org/licenses/by-sa/3.0/us/
+
+# All rights reserved.
+
+# NOTE: If you are running this in the R console you must use the 'setwd' command to set the 
+# working directory for the console to whereever you have saved this file prior to running.
+# Otherwise you will see errors when loading data or saving figures!
+#############################################################################################
+
+
+# session init ----
+rm(list=ls())
+graphics.off()
+#setwd("working directory path")
+
+# load libraries ----
+if(!require(tidyverse)) {
+  install.packages("tidyverse", repos = "http://cran.us.r-project.org")
+  library(tidyverse)
+}
+if(!require(caret)) {
+  install.packages("caret", repos = "http://cran.us.r-project.org")
+  library(caret)
+}
+
+#  load and partition data ----
+# data can be downloaded from kaggle (https://www.kaggle.com/pitasr/falldata)
+data_file_path <- 'https://raw.githubusercontent.com/mdt-ds/PH125.9_cyo_fallDetection/master/falldeteciton.csv'
+fall_detection <- read_csv(file = data_file_path)
+set.seed(525)
+train_idx <- createDataPartition(y = fall_detection$ACTIVITY, times = 1, 
+                                 p = 0.7, list = FALSE)
+fall_train <- fall_detection %>% slice(train_idx)
+fall_test <- fall_detection %>% slice(-train_idx)
+rm(data_file_path, train_idx)
+
+# exploratory data analysis ----
+
+# 0- Standing 1- Walking 2- Sitting 3- Falling 4- Cramps 5- Running
+fall_train <- fall_train %>% mutate(ACTIVITY = factor(ACTIVITY, 
+                             labels = c('Standing', 'Walking', 'Sitting', 
+                                        'Falling', 'Cramps', 'Running')))
+fall_test <- fall_test %>% mutate(ACTIVITY = factor(ACTIVITY, 
+                              labels = c('Standing', 'Walking', 'Sitting', 
+                                        'Falling', 'Cramps', 'Running')))
+
+# ADLs Distribution
+ggplot(fall_train, aes(ACTIVITY)) +
+  geom_bar(aes(fill = ACTIVITY)) +
+  labs(title = 'ADLs Distribution')
+
+# boxplot distribution by activity
+timeBP <- ggplot(fall_train, aes(ACTIVITY, TIME)) +
+  geom_boxplot(aes(fill = ACTIVITY)) +
+  ylab('monitoring time') +
+  theme(legend.position = 'none', 
+        axis.title.x = element_blank(),
+        axis.text.x = element_text(size = 7))
+
+slBP <- ggplot(fall_train, aes(ACTIVITY, SL)) +
+  geom_boxplot(aes(fill = ACTIVITY)) +
+  ylab('sugar level') +
+  theme(legend.position = 'none', 
+        axis.title.x = element_blank(),
+        axis.text.x = element_text(size = 7))
+
+eegBP <- ggplot(fall_train, aes(ACTIVITY, EEG)) +
+  geom_boxplot(aes(fill = ACTIVITY)) +
+  ylab('EEG monitoring rate') +
+  theme(legend.position = 'none',
+        axis.text.x = element_text(size = 7))
+
+bloodBP <- ggplot(fall_train, aes(ACTIVITY, BP)) +
+  geom_boxplot(aes(fill = ACTIVITY)) +
+  ylab('blood pressure') +
+  theme(legend.position = 'none', 
+        axis.title.x = element_blank(),
+        axis.text.x = element_text(size = 7))
+
+hrBP <- ggplot(fall_train, aes(ACTIVITY, HR)) +
+  geom_boxplot(aes(fill = ACTIVITY)) +
+  ylab('heart beat rate') +
+  theme(legend.position = 'none', 
+        axis.title.x = element_blank(),
+        axis.text.x = element_text(size = 7))
+
+circBP <- ggplot(fall_train, aes(ACTIVITY, CIRCLUATION)) +
+  geom_boxplot(aes(fill = ACTIVITY)) +
+  ylab('blood circulation') +
+  theme(legend.position = 'none',
+        axis.text.x = element_text(size = 7))
+
+if(!require(gridExtra)) {
+  install.packages("gridExtra", repos = "http://cran.us.r-project.org")
+  library(gridExtra)
+}
+grid.arrange(timeBP, slBP, bloodBP, hrBP, circBP, eegBP, 
+             nrow = 3, ncol = 2, newpage = TRUE)
+
+# predictor correlation
+if(!require(GGally)) {
+  install.packages("GGally", repos = "http://cran.us.r-project.org")
+  library(GGally)
+}
+ggpairs(fall_train %>% select(-ACTIVITY), axisLabels = 'none')
+
+# feature importance
+if(!require(randomForest)) {
+  install.packages("randomForest", repos = "http://cran.us.r-project.org")
+  library(randomForest)
+}
+
+rf_mdl <- randomForest(ACTIVITY ~ ., data = fall_train)
+varImpPlot(rf_mdl)
+
+# train control
+ctrl <- trainControl(method = 'cv', number = 5, p = .8)
+
+# load libraries for parallel computation
+if(!require(doParallel)) {
+  install.packages("doParallel", repos = "http://cran.us.r-project.org")
+  library(doParallel)
+}
+
+# modeling with multinomial ----
+modelLookup(model = 'multinom')
+tunegrid <- data.frame(decay = seq(from = 0, to = 10, by = 1))
+if(!require(nnet)) {
+  install.packages("nnet", repos = "http://cran.us.r-project.org")
+  library(nnet)
+}
+# setup parallel computing
+cl <- makePSOCKcluster(parallel::detectCores()-1)
+registerDoParallel(cl)
+
+# train model
+set.seed(525) 
+multinom_mdl <- train(ACTIVITY ~ .,
+                      data = fall_train,
+                      method = "multinom", 
+                      tuneGrid = tunegrid, 
+                      trControl = ctrl)
+
+#Shutdown cluster
+stopCluster(cl)
+
+plot(multinom_mdl)
+multinom_mdl$bestTune
+
+# evaluate model 
+y_hat <- predict(multinom_mdl, newdata = fall_test, type = 'raw')
+confusionMatrix(data = y_hat, reference = fall_test$ACTIVITY)$table
+test_accuracy_multinom <- confusionMatrix(data = y_hat, reference = fall_test$ACTIVITY)$overall['Accuracy']
+paste('model test accuracy:', round(test_accuracy_multinom, 5), sep = '   ')
+
+# save test results in a tibble
+test_results <- tibble(method = 'multinom', accuracy = test_accuracy_multinom)
+
+# modeling with kNN ----
+modelLookup(model = 'knn')
+tunegrid <- data.frame(k= seq(1, 30, 1))
+
+# scale data
+fall_train_norm <- predict(preProcess(fall_train, method = c("center", "scale")),
+                           newdata = fall_train)
+fall_test_norm <- predict(preProcess(fall_train, method = c("center", "scale")),
+                          newdata = fall_test)
+
+if(!require(e1071)) {
+  install.packages("e1071", repos = "http://cran.us.r-project.org")
+  library(e1071)
+}
+
+# setup parallel computing
+cl <- makePSOCKcluster(parallel::detectCores()-1)
+registerDoParallel(cl)
+
+# train model
+set.seed(525) 
+knn_mdl <- train(ACTIVITY ~ .,
+                 data = fall_train_norm,
+                 method = "knn", 
+                 tuneGrid = tunegrid, 
+                 trControl = ctrl)
+
+#Shutdown cluster
+stopCluster(cl)
+
+plot(knn_mdl)
+knn_mdl$bestTune
+
+# evaluate model 
+y_hat <- predict(knn_mdl, newdata = fall_test_norm, type = 'raw')
+confusionMatrix(data = y_hat, reference = fall_test$ACTIVITY)$table
+test_accuracy_knn <- confusionMatrix(data = y_hat, reference = fall_test$ACTIVITY)$overall['Accuracy']
+paste('model test accuracy:', round(test_accuracy_knn, 5), sep = '   ')
+
+# save test results in a tibble
+test_results <- test_results %>% add_row(method = 'knn', accuracy = test_accuracy_knn)
+
+# modeling with randomForest ----
+modelLookup(model = 'rf')
+tunegrid <- data.frame(mtry= seq(1, 5, 1))
+
+if(!require(randomForest)) {
+  install.packages("randomForest", repos = "http://cran.us.r-project.org")
+  library(randomForest)
+}
+
+# setup parallel computing
+cl <- makePSOCKcluster(parallel::detectCores()-1)
+registerDoParallel(cl)
+
+# train model
+set.seed(525) 
+rf_mdl <- train(ACTIVITY ~ .,
+                data = fall_train,
+                method = "rf", 
+                tuneGrid = tunegrid, 
+                trControl = ctrl)
+
+#Shutdown cluster
+stopCluster(cl)
+
+plot(rf_mdl)
+rf_mdl$bestTune
+
+# evaluate model 
+y_hat <- predict(rf_mdl, newdata = fall_test, type = 'raw')
+confusionMatrix(data = y_hat, reference = fall_test$ACTIVITY)$table
+test_accuracy_rf <- confusionMatrix(data = y_hat, reference = fall_test$ACTIVITY)$overall['Accuracy']
+paste('model test accuracy:', round(test_accuracy_rf, 5), sep = '   ')
+
+# save test results in a tibble
+test_results <- test_results %>% add_row(method = 'rf', accuracy = test_accuracy_rf)
+
+# results ----
+test_results %>% knitr::kable()
+
+
+# end of script ########################################################################################
